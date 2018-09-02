3b3788e7a23a5447b9afde60c1435125401bd5e7
acoutts <andrewjohncoutts@gmail.com>
2018-09-02 16:19:45 -0400

    Implement zeromq messages in place of http requests

--- watcher_plugin/watcher_plugin.cpp
+++ watcher_plugin/watcher_plugin.cpp
@@ -3,7 +3,6 @@
 *  @copyright eosauthority - free to use and modify - see LICENSE.txt
 */
 #include <eosio/watcher_plugin/watcher_plugin.hpp>
-#include <eosio/watcher_plugin/http_async_client.hpp>
 #include <eosio/chain/controller.hpp>
 #include <eosio/chain/trace.hpp>
 #include <eosio/chain_plugin/chain_plugin.hpp>
@@ -16,7 +15,13 @@
 #include <boost/algorithm/string.hpp>
 
 #include <unordered_map>
+#include <zmq.hpp>
+#include <string>
 
+namespace {
+  const char* SENDER_BIND = "zmq-sender-bind";
+  const char* SENDER_BIND_DEFAULT = "tcp://127.0.0.1:5556";
+}
 
 namespace eosio {
    static appbase::abstract_plugin& _watcher_plugin = app().register_plugin<watcher_plugin>();
@@ -33,20 +38,21 @@ namespace eosio {
       static const fc::microseconds max_deserialization_time;
 
       struct action_notif {
-         action_notif(const action& act, transaction_id_type tx_id, const variant& action_data, const uint32_t block_num)
-         : tx_id(tx_id), block_num(block_num), account(act.account), name(act.name), authorization(act.authorization),
+         action_notif(const action& act, transaction_id_type tx_id, const variant& action_data)
+         : tx_id(tx_id), account(act.account), name(act.name), authorization(act.authorization),
          action_data(action_data) {}
 
          transaction_id_type      tx_id;
-         uint32_t                 block_num;
          account_name             account;
          action_name              name;
          vector<permission_level> authorization;
          fc::variant              action_data;
       };
 
-      struct message {
-         std::vector<action_notif> actions;
+      struct  message {
+        uint32_t block_num;
+        fc::time_point timestamp;
+        std::vector<action_notif> actions;
       };
 
       struct filter_entry {
@@ -62,16 +68,21 @@ namespace eosio {
          }
       };
 
-
+      zmq::context_t context;
+      zmq::socket_t sender_socket;
       chain_plugin* chain_plug = nullptr;
       fc::optional<boost::signals2::scoped_connection> accepted_block_conn;
       fc::optional<boost::signals2::scoped_connection> applied_tx_conn;
       std::set<watcher_plugin_impl::filter_entry>      filter_on;
-      http_async_client                                httpc;
       fc::url                                          receiver_url;
       int64_t                                          age_limit = default_age_limit;
       action_queue_t                                   action_queue;
 
+
+      watcher_plugin_impl():
+        context(1),
+        sender_socket(context, ZMQ_PUSH)
+      {}
 
       bool filter( const action_trace& act ) {
          if( filter_on.find({ act.receipt.receiver, act.act.name }) != filter_on.end() )
@@ -118,7 +129,7 @@ namespace eosio {
          }
       }
 
-      void build_message(message& msg, const transaction_id_type& tx_id, uint32_t block_num) {
+      void build_message( message& msg, const transaction_id_type& tx_id) {
          //~ ilog("inside build_message - block_num: ${u}", ("u",block_num));
          auto range = action_queue.equal_range(tx_id);
          for( auto& it = range.first; it != range.second; it++) {
@@ -126,61 +137,69 @@ namespace eosio {
             //~ ilog("iterator it->first: ${u}", ("u",it->first));
             //~ ilog("iterator it->second: ${u}", ("u",it->second));
             auto act_data = deserialize_action_data(it->second);
-            action_notif notif( it->second, tx_id, std::forward<fc::variant>(act_data), block_num );
+            action_notif notif( it->second, tx_id, std::forward<fc::variant>(act_data) );
             //~ ilog("inside for loop for each action - action_notif: ${u}", ("u",notif));
             msg.actions.push_back(notif);
          }
       }
 
-      void send_message(const message& msg) {
-        //~ ilog("send_message msg: ${u}",("u",msg));
-         //~ dlog("Sending: ${a}", ("a", fc::json::to_pretty_string(msg)));
-         try{
-            httpc.post(receiver_url, msg, fc::time_point::now() + http_timeout );
-         }
-         FC_CAPTURE_AND_LOG(("Error while sending notification")(msg));
+      void send_zmq_message(const  message& msg) {
+        ilog("Sending: ${u}",("u",fc::json::to_string(msg)));
+        string zao_json = fc::json::to_string(msg);
+
+        int32_t msgtype = 0;
+        int32_t msgopts = 0;
+
+        zmq::message_t message(zao_json.length()+sizeof(msgtype)+sizeof(msgopts));
+        unsigned char* ptr = (unsigned char*) message.data();
+        memcpy(ptr, &msgtype, sizeof(msgtype));
+        ptr += sizeof(msgtype);
+        memcpy(ptr, &msgopts, sizeof(msgopts));
+        ptr += sizeof(msgopts);
+        memcpy(ptr, zao_json.c_str(), zao_json.length());
+        sender_socket.send(message);
       }
 
       void on_accepted_block(const block_state_ptr& block_state) {
-         //~ ilog("on_accepted_block | block_state->block: ${u}", ("u",block_state->block));
-         fc::time_point btime = block_state->block->timestamp;
-         if( age_limit == -1 || (fc::time_point::now() - btime < fc::seconds(age_limit)) ) {
-            message msg;
-            transaction_id_type tx_id;
-            uint32_t block_num = block_state->block->block_num();
-            //~ ilog("Block_num: ${u}", ("u",block_num));
+        //~ ilog("on_accepted_block | block_state->block: ${u}", ("u",block_state->block));
+        fc::time_point btime = block_state->block->timestamp;
+        if( age_limit == -1 || (fc::time_point::now() - btime < fc::seconds(age_limit)) ) {
+          message msg;
+          transaction_id_type tx_id;
+          uint32_t block_num = block_state->block->block_num();
+          //~ ilog("Block_num: ${u}", ("u",block_num));
 
-            //~ Process transactions from `block_state->block->transactions` because it includes all transactions including deferred ones
-            //~ ilog("Looping over all transaction objects in block_state->block->transactions");
-            for( const auto& trx : block_state->block->transactions ) {
-              if( trx.trx.contains<transaction_id_type>() ) {
-                //~ For deferred transactions the transaction id is easily accessible
-                //~ ilog("===> block_state->block->transactions->trx ID: ${u}", ("u",trx.trx.get<transaction_id_type>()));
-                tx_id = trx.trx.get<transaction_id_type>();
-              }
-              else {
-                //~ For non-deferred transactions we have to access the txid from within the packed transaction. The `trx` structure and `id()` getter method are defined in `transaction.hpp`
-                //~ ilog("===> block_state->block->transactions->trx ID: ${u}", ("u",trx.trx.get<packed_transaction>().id()));
-                tx_id = trx.trx.get<packed_transaction>().id();
-              }
-
-              //~ ilog("action_queue.size: ${u}", ("u",action_queue.size()));
-              if( action_queue.count(tx_id) ) {
-                 build_message(msg, tx_id, block_num);
-              }
+          //~ Process transactions from `block_state->block->transactions` because it includes all transactions including deferred ones
+          //~ ilog("Looping over all transaction objects in block_state->block->transactions");
+          for( const auto& trx : block_state->block->transactions ) {
+            if( trx.trx.contains<transaction_id_type>() ) {
+              //~ For deferred transactions the transaction id is easily accessible
+              //~ ilog("===> block_state->block->transactions->trx ID: ${u}", ("u",trx.trx.get<transaction_id_type>()));
+              tx_id = trx.trx.get<transaction_id_type>();
+            }
+            else {
+              //~ For non-deferred transactions we have to access the txid from within the packed transaction. The `trx` structure and `id()` getter method are defined in `transaction.hpp`
+              //~ ilog("===> block_state->block->transactions->trx ID: ${u}", ("u",trx.trx.get<packed_transaction>().id()));
+              tx_id = trx.trx.get<packed_transaction>().id();
             }
 
-            //~ ilog("Done processing block_state->block->transactions");
-
-            if( msg.actions.size() > 0 ) {
-               //~ ilog("Sending message - msg.actions.size(): ${u}",("u",msg.actions.size()));
-               send_message(msg);
+            //~ ilog("action_queue.size: ${u}", ("u",action_queue.size()));
+            if( action_queue.count(tx_id) ) {
+              build_message(msg, tx_id);
             }
-         }
-         // TODO: Leave unsent actions until they are expired or are included in future blocks?
-         action_queue.clear();
+          }
+
+          //~ ilog("Done processing block_state->block->transactions");
+
+          //~ Always make sure we send a new block notification to the watcher plugin for candlestick charting timestamps
+          msg.block_num = block_num;
+          msg.timestamp = btime;
+          send_zmq_message(msg);
+        }
+        // TODO: Leave unsent actions until they are expired or are included in future blocks?
+        action_queue.clear();
       }
-   };
+    };
 
    const fc::microseconds watcher_plugin_impl::http_timeout = fc::seconds(10);
    const fc::microseconds watcher_plugin_impl::max_deserialization_time = fc::seconds(5);
@@ -199,18 +218,32 @@ namespace eosio {
       ("watch-age-limit",
       bpo::value<int64_t>()->default_value(watcher_plugin_impl::default_age_limit),
       "Age limit in seconds for blocks to send notifications about."
-      "No age limit if set to negative.");
+      "No age limit if set to negative.")
+      (SENDER_BIND, bpo::value<string>()->default_value(SENDER_BIND_DEFAULT),
+       "ZMQ Sender Socket binding");
 
    }
 
    void watcher_plugin::plugin_initialize(const variables_map& options) {
-
       try {
          EOS_ASSERT(options.count("watch-receiver-url") == 1, fc::invalid_arg_exception,
          "watch_plugin requires one watch-receiver-url to be specified!");
 
          string url_str = options.at("watch-receiver-url").as<string>();
          my->receiver_url = fc::url(url_str);
+
+
+
+         string bind_str = options.at(SENDER_BIND).as<string>();
+         if (bind_str.empty())
+           {
+             wlog("zmq-sender-bind not specified => eosio::zmq_plugin disabled.");
+             return;
+           }
+         ilog("Binding to ${u}", ("u", bind_str));
+         my->sender_socket.bind(bind_str);
+
+
 
          if (options.count("watch")) {
             auto fo = options.at("watch").as<vector<string>>();
@@ -231,7 +264,6 @@ namespace eosio {
          if (options.count("watch-age-limit"))
          my->age_limit = options.at("watch-age-limit").as<int64_t>();
 
-
          my->chain_plug = app().find_plugin<chain_plugin>();
          auto& chain = my->chain_plug->chain();
          my->accepted_block_conn.emplace(chain.accepted_block.connect(
@@ -248,18 +280,15 @@ namespace eosio {
    }
 
    void watcher_plugin::plugin_startup() {
-      my->httpc.start();
-      ilog("Watcher plugin started");
+
    }
 
    void watcher_plugin::plugin_shutdown() {
       my->applied_tx_conn.reset();
       my->accepted_block_conn.reset();
-      my->httpc.stop();
    }
 
 }
 
-FC_REFLECT(eosio::watcher_plugin_impl::action_notif, (tx_id)(block_num)(account)(name)(authorization)
-   (action_data))
-FC_REFLECT(eosio::watcher_plugin_impl::message, (actions))
+FC_REFLECT(eosio::watcher_plugin_impl::action_notif, (tx_id)(account)(name)(authorization)(action_data))
+FC_REFLECT(eosio::watcher_plugin_impl:: message, (block_num)(timestamp)(actions))
