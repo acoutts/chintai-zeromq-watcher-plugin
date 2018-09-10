b51594d127e83fac51db1a05e9210b49222c4507
Andrew Coutts <andrewjohncoutts@gmail.com>
2018-09-10 09:11:04 -0400

    Update watcher plugin to push notifications for cancelorder actions
    Preparing the frontend for cancel order actions

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-cp -r -f ~/Projects/ChintaiWeb/nodeos-plugins/chintai-eosio-watcher-plugin/watcher_plugin ~/Projects/eoshome/eos/plugins/
+cp -r -f ~/Projects/chintai-zeromq-watcher-plugin/watcher_plugin ~/Projects/eoshome/eos/plugins/
 cd ~/Projects/eoshome/eos
 ./eosio_install.sh
